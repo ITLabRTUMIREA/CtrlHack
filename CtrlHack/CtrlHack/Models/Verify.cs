aa353948038bddbcb82ac590f1513a8a1f4ded36
Maksim Makushchenko <maksalmak@gmail.com>
2018-09-22 20:43:38 +0300

    Done first downloading logic

---
+++ CtrlHack/CtrlHack/Models/Verify.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlHack.Models
+{
+    public class Verify
+    {
+        public string OrgName { get; set; }
+        public string ORGN { get; set; }
+        public string INN { get; set; }
+        public string Address { get; set; }
+        public string Purpose { get; set; }
+        public string Date { get; set; }
+        public string Duration { get; set; }
+        public string Form { get; set; }
+        public string HardField { get; set; }
+    }
+}
