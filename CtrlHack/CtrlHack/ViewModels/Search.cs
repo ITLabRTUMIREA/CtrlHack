e7b68593d9e6878b84e0d6803ba51d1f96141b1a
Denis <winda.wzse@gmail.com>
2018-09-23 10:59:14 +0300

    Search page (doen't work)

---
+++ CtrlHack/CtrlHack/ViewModels/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CtrlHack.ViewModels
+{
+	public class Search : BaseViewModel
+	{
+		public Search ()
+		{
+            Title = "Поиск проверок";
+
+            //OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        //public ICommand OpenWebCommand { get; }
+	}
+}
