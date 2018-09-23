aea30b775f8fcc53e6f6c545b11cb74bfb168dfc
Maksim Makushchenko <maksalmak@gmail.com>
2018-09-23 14:52:31 +0300

    Search work

---
+++ CtrlHack/CtrlHack/Models/RegionPair.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlHack.Models
+{
+    public class RegionPair
+    {
+        public string Title { get; set; }
+        public int? Number { get; set; }
+    }
+}
